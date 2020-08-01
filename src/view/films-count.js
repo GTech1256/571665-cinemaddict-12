418c483a5ab80e5a68b45c3aead40787df56361c
Roman Bakakin <pomah156@gmail.com>
2020-08-01 19:53:02 +0300

    Разбивает код

---
+++ src/view/films-count.js
@@ -0,0 +1,9 @@
+const createFilmsCountTemplate = () => {
+  return (
+    `<p>130 291 movies inside</p>`
+  );
+};
+
+export {
+  createFilmsCountTemplate
+};
---
+++ src/view/films-most-commented-list.js
@@ -0,0 +1,14 @@
+const createFilmsMostCommentedListTemplate = () => {
+  return (
+    `<section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
+
+export {
+  createFilmsMostCommentedListTemplate
+};
